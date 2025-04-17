bd34c295bb3c7beb4fca0e8408fd8997d7696df3
dave8766 <david.r.bloomfield33@gmail.com>
2025-04-17 15:47:37 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Welcome to Konnect! (Styled with Tailwind)
+      </h1>
+    </div>
+  );
+}
+export default App;
